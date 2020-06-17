f6d6dd0396a82fc6b0c56f241498e78cca149d31
PedroEdu6786 <pedrcg835@gmail.com>
2020-06-16 22:56:19 -0500

    CentroComercial methods implemented

--- src/Model/CentroComercial/CentroComercial.java
+++ src/Model/CentroComercial/CentroComercial.java
@@ -5,9 +5,11 @@
  */
 package Model.CentroComercial;
 
+import Model.CarritoCompras.CarritoCompras;
 import Model.Cliente.Cliente;
 import Patrones.Iterator.ClientesRepository;
 import Patrones.Iterator.Iterator;
+import Patrones.Iterator.TiendasRepository;
 import Patrones.Observer.Observadores;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,11 +21,37 @@ import java.util.Date;
 public class CentroComercial implements Observadores {
 
     private String nombre;
-    //FALTA LISTATIENDAS
+    private TiendasRepository tiendas;
     private ClientesRepository clientes;
+    
+    public CentroComercial(String nombre) {
+        this.nombre = nombre;
+        tiendas = new TiendasRepository();
+        clientes = new ClientesRepository();
+    }
+    
+    public void entrar(Cliente cliente) {
+        clientes.addCliente(cliente);
+    }
+    
+    public void salir(Cliente cliente) {
+        clientes.removeCliente(cliente);
+    }
+    
+    public CarritoCompras getCarrito() {
+        return new CarritoCompras();
+    }
+    
+    public Iterator getTiendas() {
+        return tiendas.getIterator();
+    }
     
     public Iterator getClientes(){
         return clientes.getIterator();
+    }
+    
+    public void pagar() {
+        
     }
 
     @Override
